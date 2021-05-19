c7037bd5c7866e5b8901c5cc82ac7280d935c3e6
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-19 13:39:25 -0400

    configured mongo and .env

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/database");
+const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+require("dotenv").config({ path: "./config/.env" });
+
+connectDB();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +28,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+//Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
