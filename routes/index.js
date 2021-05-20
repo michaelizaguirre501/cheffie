321807198b904ca1a5cfc0e108e734637199e609
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-20 14:37:40 -0400

    routes for sign up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+// Get sign up page
+router.get("/signup", function (req, res, next) {
+  res.render("signup", { title: "Express" });
+});
 module.exports = router;
