3079a8d30423b50eb99b0d7ceb637bb5bb575864
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-05-21 15:23:14 -0400

    food model sign in sign up

---
+++ models/FoodItems.js
@@ -0,0 +1,30 @@
+// item name , entree app or w.e
+//ingredient meta
+// image?
+//
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: Array,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
